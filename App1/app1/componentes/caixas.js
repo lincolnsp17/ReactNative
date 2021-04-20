7c6a0ec9c671a9cad205a09fee7d63c96aa65e72
Lincoln Trabalho <lincolnsp17@gmail.com>
2021-04-20 10:47:28 -0300

    novo commit

--- App1/app1/componentes/caixas.js
+++ App1/app1/componentes/caixas.js
@@ -10,7 +10,7 @@ import {View,Text} from 'react-native';
  export default function(props){
     return(
         <View style={{width:'100%',height:300,flexDirection:'row',flexWrap:'wrap'}}>
-            <View style={{width:'100%',height:50,backgroundColor:'#00f',justifyContent:'flex-start',alignItems:'center'}}><Text>test</Text></View>
+            <View style={{width:'100%',height:50,backgroundColor:'#00f',justifyContent:'flex-start',alignItems:'center'}}></View>
             <View style={{width:'100%',height:50,backgroundColor:'#00a'}}></View>
             <View style={{width:'100%',height:50,backgroundColor:'#005'}}></View>
             <View style={{width:'100%',height:50,backgroundColor:'#f00'}}></View>
