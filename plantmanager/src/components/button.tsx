5d25bc96c95709bd19e4a4767c582f4e470ed338
Lincoln Ferreira <lincolnsp17@gmail.com>
2021-04-21 22:51:50 -0300

    mudanças de noite

--- plantmanager/src/components/button.tsx
+++ plantmanager/src/components/button.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, TouchableOpacityProps } from 'react-native';
 import colors from '../../styles/colors';
 
+interface ButtonProps extends TouchableOpacityProps{
+    title: string;
+}
 
-export function Button(){
+export function Button({ title } : ButtonProps){
     return(
-<TouchableOpacity style={styles.button} activeOpacity={0.7}>
-<Text style={styles.buttonText}>
-        >
-</Text>
-</TouchableOpacity>
+        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+            <Text style={styles.buttonText}>
+                 {title}
+            </Text>
+        </TouchableOpacity>
 
 )
 }
